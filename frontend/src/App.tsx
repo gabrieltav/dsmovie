5918202b8c815ef9be91e6790984798e0216b6d0
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-03-07 21:54:02 -0400

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
+import Navbar from 'components/Navbar';
+
 function App() {
-  return <h1>Hello DsMovie</h1>;
+  return (
+    <Navbar />
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a
+            href="https://github.com/gabrieltav"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/gabrieltav</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
