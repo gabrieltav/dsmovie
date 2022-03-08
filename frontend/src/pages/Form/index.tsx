3a4235ea4571950d83254818b194d659ec3d1274
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-03-08 09:56:16 -0400

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+const Form = () => {
+    return (
+        <h1>form</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+const Listing = () => {
+    return (
+        <h1>Pagina de listagem</h1>
+    );
+}
+
+export default Listing;
