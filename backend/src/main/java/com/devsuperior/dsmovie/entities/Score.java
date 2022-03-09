76a8cdba1441dfc792b96faf34336692c580b9e8
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-03-09 14:20:54 -0400

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,66 @@
+package com.devsuperior.dsmovie.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private ScorePK id = new ScorePK();
+	private Double value;
+
+	public Score() {
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Score other = (Score) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
